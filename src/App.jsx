5d0a1af4eaa7da3d978a253e9e173f8a87360f99
raveeshcool <raveesh_g@me.iitr.ac.in>
2024-12-23 21:27:04 +0530

    basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import { useState } from 'react'
 import './App.css'
-
+import {generateMnemonic} from 'bip39'
+import { SolanaWallet } from './components/SolanaWallet'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mnemonic, setMnemonic] = useState("")
 
   return (
-    <>
+    
       <div>
-       
+       <div><button onClick={newMn}>Create Seed Phrase </button></div>
+      
+       <div>{mnemonic}</div>
+
+       <SolanaWallet mnemonic={mnemonic} />
       </div>
-    </>
+    
   )
+
+  async function newMn(){ 
+    const mn = await generateMnemonic()
+    setMnemonic(mn)
+  }
 }
+
+
 
 export default App
